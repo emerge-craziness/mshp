01dcef5fcbfaf9e49bc695526735a30f32ea493b
Ilya Pikulin <ispikulin@gmail.com>
2015-05-13 23:04:11 +0300

    made more readable

--- 20/D.cpp
+++ 20/D.cpp
@@ -1,5 +1,4 @@
-/* Copyright 2015 by Pikulin Ilya */
-#include <iostream>
+/* Copyright 2015, by Pikulin Ilya */
 #include <cstdio>
 using namespace std;
 
@@ -8,37 +7,17 @@ using namespace std;
 int main( int argc, char** argv )
 {
     int N;
-    cin >> N;
+    scanf( "%i", &N );
     
-    int local[4];
-    
-    int counter = 0;
     for ( int i = 0; i * 10 <= N; i++ )
     {
-#ifdef DEBUGGING
-                fprintf( stderr, "%s: line %i ok\n", __FILE__, __LINE__ );
-                fflush( stderr );
-#endif
         for ( int j = 0; j * 5 <= N - i * 10; j++ )
         {
-#ifdef DEBUGGING
-                fprintf( stderr, "%s: line %i ok\n", __FILE__, __LINE__ );
-                fflush( stderr );
-#endif
             for ( int k = 0; k * 2 <= N - i * 10 - j * 5; k++ )
             {
                 int l = N - i * 10 - j * 5 - k * 2;
                 
                 printf( "%i tens %i fives %i twos %i ones\n", i, j, k, l );
-#ifdef DEBUGGING
-                fprintf( stderr, "%s: line %i ok\n", __FILE__, __LINE__ );
-                fflush( stderr );
-#endif
-#ifdef DEBUGGING
-                fprintf( stderr, "%s: line %i ok\n", __FILE__, __LINE__ );
-                fflush( stderr );
-#endif
-                
             }
         }
     }
@@ -46,9 +25,3 @@ int main( int argc, char** argv )
     return 0;
 }
 
-
-
-
-/*
-    printf( "%i tens %i fives %i twos %i ones\n", 0, 0, 0, N / 1 );
-*/

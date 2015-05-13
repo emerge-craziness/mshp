c9d377f330f250f811078d70b2b4d20a2bfed50e
Ilya Pikulin <ispikulin@gmail.com>
2015-05-13 22:50:23 +0300

    added task D

---
+++ 20/D.cpp
@@ -0,0 +1,65 @@
+/* Copyright 2015 by Pikulin Ilya */
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+//#define DEBUGGING
+
+std::vector< int >& generateSequences( int N, int by )
+{
+    vector< int >& sequences = *( new vector< int > );
+    for ( int i = 1; i * by <= N; i++ )
+    {
+        sequences.push_back( i );
+    }
+    return sequences;
+}
+
+
+int main( int argc, char** argv )
+{
+    int N;
+    cin >> N;
+    
+    int local[4];
+    
+    int counter = 0;
+    for ( int i = 0; i * 10 <= N; i++ )
+    {
+#ifdef DEBUGGING
+                fprintf( stderr, "%s: line %i ok\n", __FILE__, __LINE__ );
+                fflush( stderr );
+#endif
+        for ( int j = 0; j * 5 <= N - i * 10; j++ )
+        {
+#ifdef DEBUGGING
+                fprintf( stderr, "%s: line %i ok\n", __FILE__, __LINE__ );
+                fflush( stderr );
+#endif
+            for ( int k = 0; k * 2 <= N - i * 10 - j * 5; k++ )
+            {
+                int l = N - i * 10 - j * 5 - k * 2;
+                
+                printf( "%i tens %i fives %i twos %i ones\n", i, j, k, l );
+#ifdef DEBUGGING
+                fprintf( stderr, "%s: line %i ok\n", __FILE__, __LINE__ );
+                fflush( stderr );
+#endif
+#ifdef DEBUGGING
+                fprintf( stderr, "%s: line %i ok\n", __FILE__, __LINE__ );
+                fflush( stderr );
+#endif
+                
+            }
+        }
+    }
+    
+    return 0;
+}
+
+
+
+
+/*
+    printf( "%i tens %i fives %i twos %i ones\n", 0, 0, 0, N / 1 );
+*/
